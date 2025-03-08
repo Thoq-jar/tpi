ac5ce341e60fba687ff1d57bb0279b8804651024
thoq <tristanshaw000@gmail.com>
2025-03-08 15:51:46 -0500

    run commands in /tmp/tpi

--- src/erlang/tpi_erlang.erl
+++ src/erlang/tpi_erlang.erl
@@ -4,5 +4,11 @@
 
 run_cmd_erl(Command) ->
     CmdStr = binary_to_list(Command),
+    Dir = "/tmp/tpi",
+    case filelib:is_dir(Dir) of
+        false -> file:make_dir(Dir);
+        true -> ok
+    end,
+    ok = file:set_cwd(Dir),
     Result = os:cmd(CmdStr),
     list_to_binary(Result).
